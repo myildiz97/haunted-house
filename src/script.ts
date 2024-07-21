252766dd1dd4cadf90943f63800b274c7984a2b8
Mehmet YILDIZ <yildiz.mehmet@metu.edu.tr>
2024-07-21 21:27:24 +0300

    add textures and environmental objects

--- src/script.ts
+++ src/script.ts
@@ -1,4 +1,4 @@
-import { Scene, Mesh, SphereGeometry, MeshStandardMaterial, AmbientLight, DirectionalLight, PerspectiveCamera, WebGLRenderer, AxesHelper, PlaneGeometry, Group, BoxGeometry, Vector3, ConeGeometry } from 'three';
+import { Scene, Mesh, SphereGeometry, MeshStandardMaterial, AmbientLight, DirectionalLight, PerspectiveCamera, WebGLRenderer, AxesHelper, PlaneGeometry, Group, BoxGeometry, Vector3, ConeGeometry, TextureLoader, RepeatWrapping, SRGBColorSpace } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { Timer } from 'three/addons/misc/Timer.js'
 import GUI from 'lil-gui'
@@ -17,7 +17,108 @@ const scene = new Scene();
 
 // Axes helper
 const axesHelper = new AxesHelper(5);
-scene.add(axesHelper);
+
+// Add a GUI control for the axesHelper visibility
+const axesHelperControl = gui.add(axesHelper, 'visible').name('Axes helper').setValue(false);
+
+// Listen for changes on the visibility control
+axesHelperControl.onChange((visible: boolean) => {
+  if (visible) {
+    // If ticked, add axesHelper to the scene
+    scene.add(axesHelper);
+  } else {
+    // If unticked, remove axesHelper from the scene
+    scene.remove(axesHelper);
+  }
+});
+
+/**
+ * Textures
+ */
+const textureLoader = new TextureLoader();
+
+// Floor
+const floorAlphaTexture = textureLoader.load('./floor/alpha.jpg');
+const floorColorTexture = textureLoader.load('./floor/coast_sand_rocks_02_1k/coast_sand_rocks_02_diff_1k.jpg');
+const floorARMTexture = textureLoader.load('./floor/coast_sand_rocks_02_1k/coast_sand_rocks_02_arm_1k.jpg');
+const floorNormalTexture = textureLoader.load('./floor/coast_sand_rocks_02_1k/coast_sand_rocks_02_nor_1k.jpg');
+const floorDisplacementTexture = textureLoader.load('./floor/coast_sand_rocks_02_1k/coast_sand_rocks_02_disp_1k.jpg');
+
+floorColorTexture.colorSpace = SRGBColorSpace;
+
+floorColorTexture.repeat.set(8, 8);
+floorARMTexture.repeat.set(8, 8);
+floorNormalTexture.repeat.set(8, 8);
+floorDisplacementTexture.repeat.set(8, 8);
+
+floorColorTexture.wrapS = RepeatWrapping;
+floorColorTexture.wrapT = RepeatWrapping;
+floorARMTexture.wrapS = RepeatWrapping;
+floorARMTexture.wrapT = RepeatWrapping;
+floorNormalTexture.wrapS = RepeatWrapping;
+floorNormalTexture.wrapT = RepeatWrapping;
+floorDisplacementTexture.wrapS = RepeatWrapping;
+floorDisplacementTexture.wrapT = RepeatWrapping;
+
+// Wall
+const wallColorTexture = textureLoader.load('./wall/castle_brick_broken_06_1k/castle_brick_broken_06_diff_1k.jpg');
+const wallARMTexture = textureLoader.load('./wall/castle_brick_broken_06_1k/castle_brick_broken_06_arm_1k.jpg');
+const wallNormalTexture = textureLoader.load('./wall/castle_brick_broken_06_1k/castle_brick_broken_06_nor_1k.jpg');
+
+wallColorTexture.colorSpace = SRGBColorSpace;
+
+// Roof
+const roofColorTexture = textureLoader.load('./roof/roof_slates_02_1k/roof_slates_02_diff_1k.jpg');
+const roofARMTexture = textureLoader.load('./roof/roof_slates_02_1k/roof_slates_02_arm_1k.jpg');
+const roofNormalTexture = textureLoader.load('./roof/roof_slates_02_1k/roof_slates_02_nor_1k.jpg');
+
+roofColorTexture.colorSpace = SRGBColorSpace;
+
+roofColorTexture.repeat.set(3, 1);
+roofARMTexture.repeat.set(3, 1);
+roofNormalTexture.repeat.set(3, 1);
+
+roofColorTexture.wrapS = RepeatWrapping;
+roofColorTexture.wrapS = RepeatWrapping;
+roofARMTexture.wrapS = RepeatWrapping;
+
+// Bush
+const bushColorTexture = textureLoader.load('./bush/leaves_forest_ground_1k/leaves_forest_ground_diff_1k.jpg');
+const bushARMTexture = textureLoader.load('./bush/leaves_forest_ground_1k/leaves_forest_ground_arm_1k.jpg');
+const bushNormalTexture = textureLoader.load('./bush/leaves_forest_ground_1k/leaves_forest_ground_nor_1k.jpg');
+
+bushColorTexture.colorSpace = SRGBColorSpace;
+
+bushColorTexture.repeat.set(2, 1);
+bushARMTexture.repeat.set(2, 1);
+bushNormalTexture.repeat.set(2, 1);
+
+bushColorTexture.wrapS = RepeatWrapping;
+bushColorTexture.wrapS = RepeatWrapping;
+bushARMTexture.wrapS = RepeatWrapping;
+
+// Grave
+const graveColorTexture = textureLoader.load('./grave/plastered_stone_wall_1k/plastered_stone_wall_diff_1k.jpg');
+const graveARMTexture = textureLoader.load('./grave/plastered_stone_wall_1k/plastered_stone_wall_arm_1k.jpg');
+const graveNormalTexture = textureLoader.load('./grave/plastered_stone_wall_1k/plastered_stone_wall_nor_1k.jpg');
+
+graveColorTexture.colorSpace = SRGBColorSpace;
+
+// Add repeat wrapping
+graveColorTexture.repeat.set(0.3, 0.4);
+graveARMTexture.repeat.set(0.3, 0.4);
+graveNormalTexture.repeat.set(0.3, 0.4);
+
+// Door
+const doorColorTexture = textureLoader.load('./door/color.jpg');
+const doorAlphaTexture = textureLoader.load('./door/alpha.jpg');
+const doorAmbientOcclusionTexture = textureLoader.load('./door/ambientOcclusion.jpg');
+const doorHeightTexture = textureLoader.load('./door/height.jpg');
+const doorNormalTexture = textureLoader.load('./door/normal.jpg');
+const doorMetalnessTexture = textureLoader.load('./door/metalness.jpg');
+const doorRoughnessTexture = textureLoader.load('./door/roughness.jpg');
+
+doorColorTexture.colorSpace = SRGBColorSpace;
 
 /**
  * House
@@ -28,11 +129,28 @@ const floorDimensions = {
   height: 20
 }
 const floor = new Mesh(
-  new PlaneGeometry(floorDimensions.width, floorDimensions.height),
-  new MeshStandardMaterial()
+  new PlaneGeometry(floorDimensions.width, floorDimensions.height, 100, 100),
+  new MeshStandardMaterial(
+    {
+      // wireframe: true,
+      alphaMap: floorAlphaTexture,
+      transparent: true,
+      map: floorColorTexture,
+      aoMap: floorARMTexture,
+      roughnessMap: floorARMTexture,
+      metalnessMap: floorARMTexture,
+      normalMap: floorNormalTexture,
+      displacementMap: floorDisplacementTexture,
+      displacementScale: 0.3,
+      displacementBias: -0.2,
+    }
+  )
 );
 floor.rotation.x = - Math.PI * 0.5
 scene.add(floor);
+
+gui.add(floor.material, 'displacementScale').min(0).max(1).step(0.001).name('Displacement scale');
+gui.add(floor.material, 'displacementBias').min(-1).max(1).step(0.001).name('Displacement bias');
 
 // House container
 const house = new Group();
@@ -46,7 +164,13 @@ const wallsDimensions = {
 }
 const walls = new Mesh(
   new BoxGeometry(wallsDimensions.width, wallsDimensions.height, wallsDimensions.depth),
-  new MeshStandardMaterial()
+  new MeshStandardMaterial({
+    map: wallColorTexture,
+    aoMap: wallARMTexture,
+    roughnessMap: wallARMTexture,
+    metalnessMap: wallARMTexture,
+    normalMap: wallNormalTexture,
+  })
 );
 walls.position.y = wallsDimensions.height / 2;
 house.add(walls);
@@ -58,7 +182,13 @@ const roofDimensions = {
 }
 const roof = new Mesh(
   new ConeGeometry(roofDimensions.radius, roofDimensions.height, 4),
-  new MeshStandardMaterial()
+  new MeshStandardMaterial({
+    map: roofColorTexture,
+    aoMap: roofARMTexture,
+    roughnessMap: roofARMTexture,
+    metalnessMap: roofARMTexture,
+    normalMap: roofNormalTexture,
+  })
 );
 roof.position.y = wallsDimensions.height + roofDimensions.height / 2;
 roof.rotation.y = Math.PI * 0.25;
@@ -70,12 +200,85 @@ const doorDimensions = {
   height: 2.2
 }
 const door = new Mesh(
-  new PlaneGeometry(doorDimensions.width, doorDimensions.height),
-  new MeshStandardMaterial({ color: '#ff0000' })
+  new PlaneGeometry(doorDimensions.width, doorDimensions.height, 100, 100),
+  new MeshStandardMaterial({
+    map: doorColorTexture,
+    alphaMap: doorAlphaTexture,
+    transparent: true,
+    aoMap: doorAmbientOcclusionTexture,
+    displacementMap: doorHeightTexture,
+    normalMap: doorNormalTexture,
+    metalnessMap: doorMetalnessTexture,
+    roughnessMap: doorRoughnessTexture,
+    displacementScale: 0.15,
+    displacementBias: -0.04,
+  })
 );
 door.position.y = doorDimensions.height / 2;
 door.position.z = wallsDimensions.depth / 2 + 0.01;
 house.add(door);
+
+// Bushes
+const bushGeometry = new SphereGeometry(1, 16, 16);
+const bushMaterial = new MeshStandardMaterial({
+  map: bushColorTexture,
+  aoMap: bushARMTexture,
+  roughnessMap: bushARMTexture,
+  metalnessMap: bushARMTexture,
+  normalMap: bushNormalTexture,
+});
+
+const bush1 = new Mesh(bushGeometry, bushMaterial);
+bush1.scale.set(0.5, 0.5, 0.5);
+bush1.position.set(0.8, 0.2, 2.2);
+bush1.rotation.x = - 0.75;
+
+const bush2 = new Mesh(bushGeometry, bushMaterial);
+bush2.scale.set(0.25, 0.25, 0.25);
+bush2.position.set(1.4, 0.1, 2.1);
+bush2.rotation.x = - 0.75;
+
+const bush3 = new Mesh(bushGeometry, bushMaterial);
+bush3.scale.set(0.4, 0.4, 0.4);
+bush3.position.set(-0.8, 0.1, 2.2);
+bush3.rotation.x = - 0.75;
+
+const bush4 = new Mesh(bushGeometry, bushMaterial);
+bush4.scale.set(0.15, 0.15, 0.15);
+bush4.position.set(-1, 0.05, 2.6);
+bush4.rotation.x = - 0.75;
+
+house.add(bush1, bush2, bush3, bush4);
+
+// Graves
+const graveGeometry = new BoxGeometry(0.6, 0.8, 0.2);
+const graveMaterial = new MeshStandardMaterial({
+  map: graveColorTexture,
+  aoMap: graveARMTexture,
+  roughnessMap: graveARMTexture,
+  metalnessMap: graveARMTexture,
+  normalMap: graveNormalTexture,
+});
+
+const graves = new Group();
+scene.add(graves);
+
+for (let i = 0; i < 30; i++) {
+  const angle = Math.random() * Math.PI * 2;
+  const radius = 3 + Math.random() * 4;
+  const x = Math.sin(angle) * radius;
+  const z = Math.cos(angle) * radius;
+
+  // Grave mesh
+  const grave = new Mesh(graveGeometry, graveMaterial);
+  grave.position.set(x, Math.random() * 0.4, z);
+  grave.rotation.x = (Math.random() - 0.5) * 0.4;
+  grave.rotation.y = (Math.random() - 0.5) * 0.4;
+  grave.rotation.z = (Math.random() - 0.5) * 0.4;
+
+  // Add grave to group
+  graves.add(grave);
+}
 
 /**
  * Lights
